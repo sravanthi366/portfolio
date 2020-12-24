1b1dbff30d2c3ea167343a67669ad0735d3bb821
sravanthi366 <sravanthimca95@gmail.com>
2020-12-24 13:40:42 +0530

    Updates

---
+++ precache-manifest.b62b5425b8565cf31ef1a79d2186e4b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb93fea024264b12cbac73cfcf92bf1",
+    "url": "/sravanthi366.github.io/portfolio.git/index.html"
+  },
+  {
+    "revision": "cf84889e50603bd69401",
+    "url": "/sravanthi366.github.io/portfolio.git/static/css/main.8c975b68.chunk.css"
+  },
+  {
+    "revision": "9fbcc8f494d63656785e",
+    "url": "/sravanthi366.github.io/portfolio.git/static/js/2.50b9f261.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/sravanthi366.github.io/portfolio.git/static/js/2.50b9f261.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf84889e50603bd69401",
+    "url": "/sravanthi366.github.io/portfolio.git/static/js/main.ebd36457.chunk.js"
+  },
+  {
+    "revision": "fece1578e94e12228214",
+    "url": "/sravanthi366.github.io/portfolio.git/static/js/runtime-main.81762ec1.js"
+  },
+  {
+    "revision": "7bbbbc044fea3358cd4093ddb86d0195",
+    "url": "/sravanthi366.github.io/portfolio.git/static/media/srav.7bbbbc04.jpg"
+  }
+]);
