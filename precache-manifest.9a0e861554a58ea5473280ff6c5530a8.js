9a7c5e6d9433f7f69ce039216f552a89209249d8
sravanthi366 <sravanthimca95@gmail.com>
2020-12-24 22:30:24 +0530

    Updates

---
+++ precache-manifest.9a0e861554a58ea5473280ff6c5530a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280a48352b1f9e6296223e91c8d6f872",
+    "url": "/portfolio-sravanthi/public/index.html"
+  },
+  {
+    "revision": "c1cc2d8be2313cd2d2bb",
+    "url": "/portfolio-sravanthi/public/static/css/main.8c975b68.chunk.css"
+  },
+  {
+    "revision": "6e3b55c61c933898bba4",
+    "url": "/portfolio-sravanthi/public/static/js/2.b5a91a1e.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/portfolio-sravanthi/public/static/js/2.b5a91a1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1cc2d8be2313cd2d2bb",
+    "url": "/portfolio-sravanthi/public/static/js/main.896dddf8.chunk.js"
+  },
+  {
+    "revision": "d46655e92b3a364ab230",
+    "url": "/portfolio-sravanthi/public/static/js/runtime-main.33be11e9.js"
+  },
+  {
+    "revision": "7bbbbc044fea3358cd4093ddb86d0195",
+    "url": "/portfolio-sravanthi/public/static/media/srav.7bbbbc04.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-sravanthi/precache-manifest.baee967b2ecac614deacfbc52440a53a.js"
+  "/portfolio-sravanthi/public/precache-manifest.9a0e861554a58ea5473280ff6c5530a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-sravanthi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-sravanthi/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
