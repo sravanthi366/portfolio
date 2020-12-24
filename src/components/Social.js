97e9a1bb2ab72ae59433f1f34d899a64abcbf377
sravanthi366 <sravanthimca95@gmail.com>
2020-12-24 21:15:30 +0530

    portfolio -6

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Social extends Component {
     render() {
         return (
-            <div class="social">
+            <div className="social">
            
-            <a href="https://github.com/sravanthi366" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://Linkedin.com/in" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/sravanthi366" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
            
             </div>
             )
